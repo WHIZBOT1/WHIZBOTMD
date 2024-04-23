b6f6ff3cba08c4426167ee9f0ce726520b608c1e
Unknownbotic <165851117+Unknownbotic@users.noreply.github.com>
2024-04-22 20:35:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = require(__dirname + '/lib/runner')
 const { VERSION } = require(__dirname + '/config')
 
 const start = async () => {
-    Debug.info(`Asta ${VERSION}`)
+    Debug.info(`whizbot ${VERSION}`)
   try {
     await bot.init()
     bot.logger.info('⏳ Starting App!')
