ad68f895d767e3b4c1b51be995a49c50ebc9599a
WHIZBOT-Md <167946140+WHIZBOT1@users.noreply.github.com>
2024-06-16 07:07:09 -0500

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -1,25 +1,4 @@
 
-const { isUserBanned } = require('./banManager');
-
-// Example of a general command
-async function someCommand(client, message) {
-    const { remoteJid, participant } = message;
-    if (isUserBanned(participant)) {
-        await client.sendMessage(remoteJid, { text: 'You are banned from using this bot.' });
-        return;
-    }
-
-    // Your command logic here
-    await client.sendMessage(remoteJid, { text: 'Command executed successfully.' });
-}
-
-module.exports = someCommand;
-
-
-
-
-
-
 global.warncount = process.env.WARN_COUNT || global.warncount || "3";
 global.MsgsInLog = process.env.MSGS_IN_LOG || global.MsgsInLog || "false";
 const { updateProfilePicture, parsedJid } = require("../lib");
