e73a20bc544465d17be55883d77db12a0840119a
WHIZBOT-Md <167946140+WHIZBOT1@users.noreply.github.com>
2024-06-29 23:41:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ global.SESSION_ID =
   ""
 module.exports = {
   menu: process.env.MENU || "",
-  HANDLERS: process.env.PREFIX || "",
+  HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "©️ 𝛲𝛩𝑊𝛯𝑅𝐷 𝐵𝑌 𝑊𝛨𝛪𝛧𝐵𝛩𝑇-𝛭𝐷",
